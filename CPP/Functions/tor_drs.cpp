17f5a8c28c7ef98e843348c7340349633e7d449f
Robert Stephany <robert.stephany@utexas.edu>
2018-08-27 21:32:55 -0500

    Improved runtime for tor_drs
    See google drive for details. I was able to improve runtime by a factor of 2.6 (a runtime reduction of 62%).

--- CPP/Functions/tor_drs.cpp
+++ CPP/Functions/tor_drs.cpp
@@ -8,86 +8,71 @@ double tor_drs(const Matrix3d & LT0T,  const Matrix3d & LTt,    const Matrix3d &
 							 const Matrix3d & LTtrs, const Matrix3d & LTt1rs, const Matrix3d & RT,    const Matrix3d & Rt,
 							 const Matrix3d & RT1,   const Matrix3d & Rt1,    const Matrix3d & Rtr,   const Matrix3d & Rts,
 							 const Matrix3d & Rt1r,  const Matrix3d & Rt1s,   const Matrix3d & Rtrs,  const Matrix3d & Rt1rs,
-							 const Matrix3d & A0,    const double beta,       const double alpha ) {
-	double scalar;
-	Vector3d v1, v2, u1, u2, v3, v4, u3, u4;
-	Matrix3d m1, m2, m3, m4;
+							 const Matrix3d & A0,    const int beta,          const int alpha ) {
+
+	// Local variables
+	const Matrix3d RT_LT0T = RT*LT0T;
+	const Matrix3d RT1_LT0T = RT1*LT0T;
+	const Matrix3d RT_LT0T1 = RT*LT0T1;
+	const Matrix3d Rtrs_LTt_plus_Rts_LTtr_plus_Rtr_LTts_plus_Rt_LTtrs = Rtrs*LTt + Rts*LTtr + Rtr*LTts + Rt*LTtrs;
+	const Matrix3d Rtr_LTt_plus_Rt_LTtr = Rtr*LTt + Rt*LTtr;
+	const Matrix3d Rts_LTt = Rts*LTt;
+	const Matrix3d Rt_LTts = Rt*LTts;
 
 	// Calculate v1, u1
-  m1 = Rt1rs*LTt*RT*LT0T
-	   + Rt1s*LTtr*RT*LT0T
-		 + Rt1r*LTts*RT*LT0T
-		 + Rt1*LTtrs*RT*LT0T;
+  Matrix3d m1 = (Rt1rs*LTt
+	    + Rt1s*LTtr
+		  + Rt1r*LTts
+		  + Rt1*LTtrs)*RT_LT0T;
 
-	m2 = Rtrs*LTt1*RT*LT0T
-	   + Rts*LTt1r*RT*LT0T
-		 + Rtr*LTt1s*RT*LT0T
-		 + Rt*LTt1rs*RT*LT0T;
+	Matrix3d m2 = (Rtrs*LTt1
+	    + Rts*LTt1r
+		  + Rtr*LTt1s
+		  + Rt*LTt1rs)*RT_LT0T;
 
-	m3 = Rtrs*LTt*RT1*LT0T
-	   + Rts*LTtr*RT1*LT0T
-		 + Rtr*LTts*RT1*LT0T
-		 + Rt*LTtrs*RT1*LT0T;
+	Matrix3d m3 = (Rtrs_LTt_plus_Rts_LTtr_plus_Rtr_LTts_plus_Rt_LTtrs)*RT1_LT0T;
 
-	m4 = Rtrs*LTt*RT*LT0T1
-	   + Rts*LTtr*RT*LT0T1
-		 + Rtr*LTts*RT*LT0T1
-		 + Rt*LTtrs*RT*LT0T1;
+	Matrix3d m4 = (Rtrs_LTt_plus_Rts_LTtr_plus_Rtr_LTts_plus_Rt_LTtrs)*RT_LT0T1;
 
-	v1 = (m1 + m2 + m3 + m4)*A0.col(beta - 1);
-	u1 = (Rt*LTt*RT*LT0T)*A0.col(alpha - 1);
+	Vector3d v1 = (m1 + m2 + m3 + m4)*A0.col(beta - 1);
+	Vector3d u1 = (Rt*LTt*RT_LT0T)*A0.col(alpha - 1);
 
 
 	// Calculate v2, u2
-  m1 = Rt1r*LTt*RT*LT0T
-		 + Rt1*LTtr*RT*LT0T;
+  m1 = (Rt1r*LTt + Rt1*LTtr)*RT_LT0T;
 
-	m2 = Rtr*LTt1*RT*LT0T
-	   + Rt*LTt1r*RT*LT0T;
+	m2 = (Rtr*LTt1 + Rt*LTt1r)*RT_LT0T;
 
-	m3 = Rtr*LTt*RT1*LT0T
-	   + Rt*LTtr*RT1*LT0T;
+	m3 = (Rtr_LTt_plus_Rt_LTtr)*RT1_LT0T;
 
-	m4 = Rtr*LTt*RT*LT0T1
-	   + Rt*LTtr*RT*LT0T1;
+	m4 = (Rtr_LTt_plus_Rt_LTtr)*RT_LT0T1;
 
-	v2 = (m1 + m2 + m3 + m4)*A0.col(beta - 1);
-	u2 = (Rts*LTt*RT*LT0T
-		 + Rt*LTts*RT*LT0T)*A0.col(alpha - 1);
+	Vector3d v2 = (m1 + m2 + m3 + m4)*A0.col(beta - 1);
+	Vector3d u2 = ((Rts_LTt + Rt_LTts)*RT_LT0T)*A0.col(alpha - 1);
 
 
 	// Calculate v3, u3
-	m1 = Rt1s*LTt*RT*LT0T
-	   + Rts*LTt1*RT*LT0T
-		 + Rt1*LTts*RT*LT0T
-		 + Rt*LTt1s*RT*LT0T;
+	m1 = (Rt1s*LTt
+	    + Rts*LTt1
+		  + Rt1*LTts
+		  + Rt*LTt1s)*RT_LT0T;
 
-	m2 = Rts*LTt*RT1*LT0T
-		 + Rts*LTt*RT*LT0T1
-		 + Rt*LTts*RT1*LT0T
-		 + Rt*LTts*RT*LT0T1;
+	m2 = Rts_LTt*(RT1_LT0T + RT_LT0T1)
+		 + Rt_LTts*(RT1_LT0T + RT_LT0T1);
 
-	v3 = (m1 + m2)*A0.col(beta - 1);
-	u3 = (Rtr*LTt*RT*LT0T
-		 + Rt*LTtr*RT*LT0T)*A0.col(alpha - 1);
+	Vector3d v3 = (m1 + m2)*A0.col(beta - 1);
+	Vector3d u3 = ((Rtr_LTt_plus_Rt_LTtr)*RT_LT0T)*A0.col(alpha - 1);
 
 
 	// Calculate v4, u4
-  m1 = Rt1*LTt*RT*LT0T
-	   + Rt*LTt1*RT*LT0T;
+  m1 = (Rt1*LTt + Rt*LTt1)*RT_LT0T;
 
-	m2 = Rt*LTt*RT1*LT0T
-	   + Rt*LTt*RT*LT0T1;
+	m2 = Rt*LTt*(RT1_LT0T + RT_LT0T1);
 
-	v4 = (m1 + m2)*A0.col(beta - 1);
-	u4 = (Rtrs*LTt*RT*LT0T
-		 + Rts*LTtr*RT*LT0T
-		 + Rtr*LTts*RT*LT0T
-		 + Rt*LTtrs*RT*LT0T)*A0.col(alpha-1);
+	Vector3d v4 = (m1 + m2)*A0.col(beta - 1);
+	Vector3d u4 = ((Rtrs_LTt_plus_Rts_LTtr_plus_Rtr_LTts_plus_Rt_LTtrs)*RT_LT0T)*A0.col(alpha-1);
 
 
 	// Calculate result
-	scalar = v1.dot(u1) + v2.dot(u2) + v3.dot(u3) + v4.dot(u4);
-
-	return scalar;
-}
+	return v1.dot(u1) + v2.dot(u2) + v3.dot(u3) + v4.dot(u4);
+} // double tor_drs(const Matrix3d & LT0T,  const Matrix3d & LTt,    const Matrix3d & LT0T1, const Matrix3d & LTt1,
