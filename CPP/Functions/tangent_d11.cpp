513ec2e6283d71988b3565d0836c53372d97e797
Robert Stephany <robert.stephany@utexas.edu>
2018-08-28 12:43:24 -0500

    Improved tangent_d11
    made return variable a reference argument

--- CPP/Functions/tangent_d11.cpp
+++ CPP/Functions/tangent_d11.cpp
@@ -2,10 +2,17 @@
 #include"Eigen/Dense"
 using namespace Eigen;
 using namespace std;
-Vector3d tangent_d11( Vector3d &a1,Vector3d &a11, Vector3d &a111, double norm_a1 )
-{
-	Vector3d t;
-	double n = norm_a1;
-	t = a111/n - a1.dot(a11)*a11/pow(n,3)+3*pow(a1.dot(a11),2)*a1/pow(n,5)-(a1.dot(a111)+a11.dot(a11))*a1/pow(n,3)-a1.dot(a11)*a11/pow(n,3);
-	return t;
-}
+void tangent_d11(const Vector3d & a1, const Vector3d & a11, const Vector3d & a111, const double norm_a1,
+	               Vector3d & t) {
+	const double n = norm_a1;
+	const double Inv_n = 1/n;
+	const double Inv_n_squared = Inv_n*Inv_n;
+	const double Inv_n_cubed = Inv_n_squared*Inv_n;
+	const double a1_dot_a11 = a1.dot(a11);
+
+	t = a111*Inv_n
+	  - a1_dot_a11*a11*Inv_n_cubed
+		+ 3*pow(a1_dot_a11,2)*a1*(Inv_n_squared*Inv_n_cubed)
+		- (a1.dot(a111) + a11.dot(a11))*a1*Inv_n_cubed
+		- a1_dot_a11*a11*Inv_n_cubed;
+} // void tangent_d11(const Vector3d & a1, const Vector3d & a11, const Vector3d & a111, const double norm_a1,
